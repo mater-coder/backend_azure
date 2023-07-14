dd2cd728e666c2c63b72621e058eb7641c95ac60
ashutoshpandey03 <ashutosh.pandey03@nagarro.com>
2023-07-14 18:25:40 +0530

    final commit

--- backend_azure/src/main/java/com/nagarro/BackendApplication/dao/AuthorDao.java
+++
@@ -1,9 +0,0 @@
-package com.nagarro.BackendApplication.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nagarro.BackendApplication.entities.Author;
-
-public interface AuthorDao extends JpaRepository<Author,Integer> {
-	Author findByName(String name);
-}
--- backend_azure/src/main/java/com/nagarro/BackendApplication/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.nagarro.BackendApplication.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nagarro.BackendApplication.entities.User;
-
-public interface UserDao extends JpaRepository<User,String> {
-
-}
